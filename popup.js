79901dc0469bde16c1cc06ec98a50a2f0d8c6d4e
dingari <danielgretars@gmail.com>
2016-06-06 22:19:04 +0000

    first commit, basic functionality implemented

---
+++ popup.js
@@ -0,0 +1,90 @@
+var binary = document.getElementById("binary");
+var decimal = document.getElementById("decimal");
+var hex = document.getElementById("hex");
+var delCheckbox = document.getElementById("del");
+
+var KEY_CTRL = 17;
+var KEY_A = 65;
+var keydown = [];
+
+var KEY_STATE_CTRL_A = false;
+
+window.addEventListener('keydown', function(e) {
+	keydown[e.keyCode] = true;
+	KEY_STATE_CTRL_A = keydown[KEY_CTRL] && e.keyCode === KEY_A;
+});
+
+window.addEventListener('keyup', function(e) {
+	keydown[e.keyCode] = false;
+});
+
+binary.addEventListener('keyup', function() {
+	var binval = validate(this.value, /[01]*/);
+	
+	setViewValue(decimal, parseInt(binval, 2));
+	setViewValue(hex, dec2hex(decimal.value));
+});
+
+decimal.addEventListener('keyup', function() {
+	var decval = validate(this.value, /[0-9]*/);
+	
+	setViewValue(binary, dec2bin(decval));
+	setViewValue(hex, dec2hex(decval));
+});
+
+hex.addEventListener('keyup', function(e) {
+	console.log(KEY_STATE_CTRL_A);
+	if(KEY_STATE_CTRL_A)
+		return;
+	
+	var hexval = validate(purify(this.value), new RegExp('[0-9a-f]*', 'i'));
+	
+	setViewValue(hex, this.value);
+	setViewValue(decimal, parseInt(hexval, 16));
+	setViewValue(binary, dec2bin(decimal.value));
+});
+
+delCheckbox.addEventListener('click', function() {
+	if(this.checked)
+		setViewValue(hex, delimiterify(hex.value));
+	else
+		setViewValue(hex, purify(hex.value));
+});
+
+function setViewValue(view, value) {
+	view.value = value || '';
+}
+
+function dec2hex(dec) {
+	var asInt = parseInt(dec);
+	if(asInt)
+		return asInt.toString(16).toUpperCase();
+}
+
+function dec2bin(dec) {
+	var asInt = parseInt(dec);
+	if(asInt)
+		return asInt.toString(2);
+}
+
+function validate(value, regex) {
+	if(value.toString().match(regex)[0] === value.toString())
+		return value;
+}
+
+function purify(hex) {
+	return hex.replace(/[-:\s]/g, '');
+}
+
+function delimiterify(hex) {
+	var newhex = "";
+	hex = hex.length % 2 ? '0' + hex : hex;
+	
+	for(var i=0; i<hex.length; i++) {
+		if(i>0 && i % 2 === 0)
+			newhex += ":";
+		newhex += hex[i];
+	}
+	
+	return newhex;
+}
